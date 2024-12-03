86b4ab27ecbd7e96ce83104019ec20e813ae9921
Saksham Khare <sakshamkhare19@gmail.com>
2024-12-03 21:40:15 +0000

    functions for route in express server

---
+++ backend/src/controller/index.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+
+export const templateController = async (req: Request, res: Response) => {};
+
+export const chatController = async (req: Request, res: Response) => {};
