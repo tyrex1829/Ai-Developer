90e840e8b74333d2a20086bb6acbaed415ef645e
Saksham Khare <sakshamkhare19@gmail.com>
2024-12-03 21:38:39 +0000

    index file of server with main-router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,19 @@
 import express from "express";
+import mainRouter from "./routes/mainRoute.js";
+import { PORT } from "./config/env.js";
 
 const app = express();
-const port = 3000;
+const port = PORT || 3000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Welcome to the server",
   });
 });
+
+app.use("/api", mainRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
