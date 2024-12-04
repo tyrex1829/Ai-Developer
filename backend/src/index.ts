b0779931d59022e9205c17b9aa7907787a838c11
Saksham Khare <sakshamkhare19@gmail.com>
2024-12-04 06:06:39 +0530

    cors package install

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import mainRouter from "./routes/mainRoute.js";
 import { PORT } from "./config/env.js";
+import cors from "cors";
 
 const app = express();
 const port = PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).json({
