f3cde9dea4b9c46ff19e08eb71c96333e0fa8ded
Saksham Khare <sakshamkhare19@gmail.com>
2024-12-07 04:12:36 +0530

    complete project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    exclude: ["lucide-react"],
+  },
+  server: {
+    watch: {
+      usePolling: true, // Enable polling for file changes
+      interval: 100, // Adjust the polling interval (default is 100ms)
+    },
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
+});
